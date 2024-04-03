d904726c34b8b0dd2f3faac970ea0ff5d1356a92
nsdfxela nsdfxela <alexander.fedosin@itseez3d.com>
2024-04-03 16:07:49 +0300

    revisited

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,12 +16,12 @@ const config = {
   url: 'https://nsdfxela.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/mpdocs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'nsdfxela', // Usually your GitHub org/user name.
-  projectName: 'mpcdocs', // Usually your repo name.
+  projectName: 'mpdocs', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
