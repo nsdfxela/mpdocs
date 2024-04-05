c4dbe9d2a8b839d4b4308f1cb8136b3a7d055eb6
nsdfxela nsdfxela <alexander.fedosin@itseez3d.com>
2024-04-05 14:54:53 +0300

    copyright

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -108,7 +108,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} itSeez3D. Built with Docusaurus.`,
       },
       prism: {
         theme: prismThemes.github,
